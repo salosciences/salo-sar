a1996fbfbb46a89d802b341ddc5aae82fd1964a2
James-Ash20 <James.ash2016@gmail.com>
2021-01-27 07:32:09 -0800

    FSH bug fixing complete. Run through good to go

--- Setup.sh
+++ Setup.sh
@@ -16,17 +16,16 @@ fi
 
 cd ".."
 
-if test -d "SAR_CH2";then
+if test -d "SAR_CH4";then
     echo "Directory exists"
 else
-    mkdir "SAR_CH2"
+    mkdir "SAR_CH4"
 fi
 
-cd "SAR_CH2"
+cd "SAR_CH4"
 
-if test -d "Data_ch2_E2"; then
+if test -d "test_example_ROIPAC"; then
     echo "Directory exists"
 else
-    wget https://gis1.servirglobal.net/TrainingMaterials/SAR/Data_ch2_E2.zip
-    unzip Data_ch2_E2.zip
+
 fi
